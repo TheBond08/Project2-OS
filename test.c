9a5c730ee2068e9e7dabd4cb1992e145e2ee8479
TheBond08 <bontislefteris@gmail.com>
2023-11-20 21:50:23 +0200

    Test

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char* argv[]){
+
+    FILE *file1;
+    printf("Error %s cannot be opened or doesn't exist.\n", file1);
+
+    return 0; 
+}
