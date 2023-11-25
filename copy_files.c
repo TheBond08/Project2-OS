dee9a3cb8c2cf87cd5d03ec3396e2650c239f45e
TheBond08 <bontislefteris@gmail.com>
2023-11-25 20:00:22 +0200

    fixes

--- copy_files.c
+++ copy_files.c
@@ -2,34 +2,36 @@
 
 
 int copy_files(int argc, char **argv){
-    int sf, df, size = 2000;
+    int sf, df, size = 0;
     char *buf = (char *)malloc(size * sizeof(char));
 
-
-    // Checking for errors
-    if((sf = open(argv[1], O_RDONLY)) < 0){
-        perror("Error opening source file:");
-        exit(1);
+    // Inputs the buffer size
+    if(scanf("%d", &size) <= 0){
+        perror("Error invalid buffer size");
+        return 1;
     }
 
-    if((df = open(argv[2], O_WRONLY | O_CREAT, 0600)) < 0){
-        perror("Error opening destination file:");
-        exit(1);
-    }
+    if(argc >= 2){ 
+        // Checking for errors
+        if((sf = open(argv[1], O_RDONLY)) < 0){
+            perror("Error opening source file:");
+            exit(1);
+        }
 
-    size_t bytesRead;
-    while ((bytesRead = fread(buf, sizeof(char), size, sf)) > 0) {
-        fwrite(buf, sizeof(char), bytesRead, df);
+        if((df = open(argv[2], O_WRONLY | O_CREAT, 0600)) < 0){
+            perror("Error opening destination file:");
+            exit(1);
+        }
+
     }
 
     // Copying the file
-    //read(sf, buf, size);
-    //write(df, buf, size);
+    read(sf, buf, size);
+    write(df, buf, size);
 
     close(sf);
     close(df);
 
-    free(buf);
 
     return sf;
 }
